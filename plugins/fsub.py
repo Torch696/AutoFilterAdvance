6790f89409d6cffd362f0011248b16859bfc3c20
Torch696 <magdum3375@gmail.com>
2024-06-10 00:32:15 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -109,8 +109,8 @@ async def ForceSub(bot: Client, update: Message, file_id: str = False, mode="che
             [
                 InlineKeyboardButton(" 🔄 Try Again 🔄 ", callback_data=f"{mode}#{file_id}")
             ],
-            [   InlineKeyboardButton("Update", url="https://t.me/VJ_Botz"),
-                InlineKeyboardButton("YouTube", url="https://youtube.com/@Tech_VJ")
+            [   InlineKeyboardButton("Update", url="https://t.me/tgflixup"),
+                InlineKeyboardButton("Channel", url="https://t.me/TgXflix")
             ]
         ]
         
