f285367fc3109c4f06c2f802c69becec1f77751f
Torch696 <magdum3375@gmail.com>
2024-06-10 00:37:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '26337689'))
 API_HASH = environ.get('API_HASH', '22cd104354b08dd79d0fb3082366c4b7') 
-BOT_TOKEN = environ.get('BOT_TOKEN', "6992277663:AAEjiKbevlQeTzbT33azbX7zWwCF5x6Ubvo") 
+BOT_TOKEN = environ.get('BOT_TOKEN', "") 
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -30,7 +30,7 @@ auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://melomagic786:JFSzeLVpVHc0FS4l@cluster0.7tubidf.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
